c5480662dee1985a8abef836c4cf231099b622db
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-12-08 10:55:53 +0530

    made update to package using script

--- src/events/base-publisher.ts
+++ src/events/base-publisher.ts
@@ -1,4 +1,4 @@
-import { Admin, Kafka, Producer } from "kafkajs";
+import { Producer } from "kafkajs";
 import { Subjects } from "./subjects";
 
 interface Event{
@@ -7,48 +7,23 @@ interface Event{
 }
 
 export abstract class Publisher<T extends Event>{
-  private client : Admin;
-  private producer :Producer;
+  private producer: Producer;
   abstract topicName : T["subject"];  
   
-  constructor(kafka:Kafka){
-    this.client = kafka.admin();
-    this.producer = kafka.producer();
-  }
-  
-  async connectProducer() {
-    await this.producer.connect();
-    console.log("Producer connected ");
-  }
-
-  setupTopic = async () => {
-    await this.client.connect();
-    await this.client.createTopics({
-      topics: [
-        {
-          topic: this.topicName,
-          numPartitions: 3,         // Number of partitions (adjust as needed)
-          replicationFactor: 1,     // Ensure compatibility with a single broker
-        },
-      ],
-    });
-    await this.client.disconnect();
+  constructor(producer: Producer) {
+    this.producer = producer;
   }
 
   async publishMessage(message: T["data"]) {
     try {
       await this.producer.send({
-        topic: this.topicName,
+        topic: this.topicName, // Uses the subject defined in the subclass
         messages: [{ value: JSON.stringify(message) }],
       });
-      console.log(`Published message to ${this.topicName}:`, message);
+      console.log(`Message published to ${this.topicName}:`, message);
     } catch (error) {
-      console.error("Error in publishing:", error);
+      console.error(`Error publishing to ${this.topicName}:`, error);
     }
   }
 
-  async disconnectProducer() {
-    await this.producer.disconnect();
-    console.log("Producer disconnected");
-  }
 } 
