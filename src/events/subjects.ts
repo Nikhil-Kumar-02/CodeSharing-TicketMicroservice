84a8394d5dbe638effc33243fc2409dbbe2bc0e7
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-12-30 07:59:08 +0530

    made update to package using script

--- src/events/subjects.ts
+++ src/events/subjects.ts
@@ -5,5 +5,7 @@ export enum Subjects{
   OrderCreated = "Order-Created",
   OrderCancelled = "Order-Cancelled",
 
-  ExpirationComplete = "Expiration-Completed"
+  ExpirationComplete = "Expiration-Completed",
+
+  PaymentCreated = "Payment-Created"
 }
